99bd823e894f0bf930ac5fd46b123d0905074ba5
Jeremy <jjp015@ucsd.edu>
2018-12-19 03:19:09 -0800

    Added short answer template and started functionality

--- app/src/main/java/com/example/jeremy/youquiz/QuizActivity.java
+++ app/src/main/java/com/example/jeremy/youquiz/QuizActivity.java
@@ -3,9 +3,26 @@ package com.example.jeremy.youquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton, mFalseButton;
+    private RadioGroup mMultipleChoice;
+    private CheckBox mMultipleAnswer;
+    private EditText mShortAnswer;
+    private ImageButton mPrevButton, mNextButton;
+
+    private int mCurrentIndex = 0;
+    private boolean mAllAnswered = false;
+    private double mScore = 0;
+
+    public QuizActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
